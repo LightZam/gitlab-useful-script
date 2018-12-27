c95401f2d83259b5821def95935e76a0fdcceaae
Zam <love8879201@gmail.com>
2018-12-27 15:08:19 +0800

    [NEW] add beginningMilestone support [MISC] remove default team

--- MVPCount.py
+++ MVPCount.py
@@ -6,18 +6,25 @@ def fetch(url):
     return response
 
 def updateMemberMVPCount(team, found):
+    if not found in team:
+        team[found] = 0
     for name in team.keys():
         if name.lower() in found.lower():
             team[name] += 1
 
 def printMVPList():
     URL = 'https://{gitlab url}/api/v4/groups/486/milestones?private_token={token}'
-    team = {'Zam': 0, 'Vantist': 0, 'Henry': 0, 'Ian': 0, 'Phate': 0, 'Jason': 0}
+    beginningMilestoneId = 0
+    team = {}
 
     # fetch milestone data
     milestones = fetch(URL).json()
     for milestone in milestones:
-        # fromat: **Sprint MVP - Zam** or ** Sprint MVP - Zam **
+        # count only after beginningMilstoneId
+        if milestone['iid'] < beginningMilstoneId:
+            continue
+
+        # fromat: Sprint MVP - Zam
         match = re.search(r'Sprint MVP - (\w*)', str(milestone['description']))
         if bool(match):
             foundMember = match.group(1)
